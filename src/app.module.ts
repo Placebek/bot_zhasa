a794666e0b2c3b955c1f40e1e9ed35c5152a4c82
sakenzi <ermekulysaken8@gmail.com>
2025-02-03 20:57:43 +0600

    sequelize migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './user/user.model';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'saken.2020',
-      database: 'chat',
-      models: [User],
-      autoLoadModels: false,
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        models: [User],
+        autoLoadModels: false, 
+      }),
+      inject: [ConfigService],
     }),
     UserModule,
   ],
